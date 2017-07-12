b9c00515d37198315449c3d427ed2c9c7ed34146
Tony Fujs <tony.fujs@gmail.com>
2017-07-12 10:55:29 -0400

    add add_search_tags()

---
+++ R/add_search_tags.R
@@ -0,0 +1,26 @@
+#' add_search_tags
+#'
+#' Add search tags to the list of variable to be added to DDH
+#'
+#' @param metadata_list list: List of metadata pulled from Microdata API
+#' @param id character: Microdata study internal ID
+#' @param token character: Microdata API authentication token
+#' @param ... ...: Extra paramter to be passed to add_search_tags (See get_md_dictionary() limit parameter)
+#'
+#' @return
+#' @export
+#'
+
+add_search_tags <- function(metadata_list, id, token, ...) {
+  # Get metadata keywords
+  keywords <- unlist(metadata_list$`codeBook/stdyDscr/stdyInfo/subject/keyword`)
+  keywords <- stringr::str_replace_all(keywords, pattern = ' +', replacement = ' ')
+  keywords <- stringr::str_replace_all(keywords, pattern = ', ?', replacement = ';')
+  # Get codebook words
+  dico <- get_md_dictionary(id = id, token = token, limit = limit)
+  # Combine
+  out <- paste(keywords, dico, sep = ';')
+  out <- stringr::str_replace(out, pattern = '^;', replacement = '')
+
+  return(out)
+}
