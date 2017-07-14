85c229a2a48e0f7fd5885e1a3544e7185bacfdbf
Tony Fujs <tony.fujs@gmail.com>
2017-07-14 10:42:33 -0400

    fix extra semi-colon issue

--- R/flatten_md_metadata.R
+++ R/flatten_md_metadata.R
@@ -2,13 +2,16 @@
 #'
 #' Flatten list
 #'
-#' @param metadata_in list: Output of mdlibtoddh::extract_md_metadata
+#' @param metadata_list list: Output of mdlibtoddh::extract_md_metadata
+#' @param multiple_fields character: JSON array fields in Microdata
+#' @param email_fields character: email fiels
+#' @param name_fields character: Name fields
 #'
 #' @return list
 #' @export
 #'
 
-flatten_md_metadata <- function(metadata_in,
+flatten_md_metadata <- function(metadata_list,
                                 multiple_fields = c("field_wbddh_primary_investigator",
                                                     "field_wbddh_authority_name",
                                                     "field_wbddh_publisher_name"),
@@ -18,48 +21,51 @@ flatten_md_metadata <- function(metadata_in,
                                                 'field_wbddh_data_collector')) {
 
   # Handle shallow indexes
-  shallow <- purrr::map_int(metadata_in, purrr::depth) == 2
-  metadata_in[shallow] <- purrr::flatten(metadata_in[shallow])
+  shallow <- purrr::map_int(metadata_list, purrr::depth) == 2
+  metadata_list[shallow] <- purrr::flatten(metadata_list[shallow])
 
   # Handle variables pulling for multiples JSON keys
-  multiple_fields <- multiple_fields[multiple_fields %in% names(metadata_in)]
+  multiple_fields <- multiple_fields[multiple_fields %in% names(metadata_list)]
 
-  metadata_in[multiple_fields] <- purrr::map(metadata_in[multiple_fields], function(x) {
-    paste(purrr::map(x, extract_from_list, keys = c('name', 'affiliation')), collapse = '; ')
+  metadata_list[multiple_fields] <- purrr::map(metadata_list[multiple_fields], function(x) {
+    out <- paste(purrr::map(x, extract_from_list, keys = c('name', 'affiliation')), collapse = '; ')
+    out <- stringr::str_replace_all(out, pattern = '^; ?|;$|; $', replacement = '')
   })
 
-  # Handle variables pulling for multiples JSON keys
-  email_fields <- email_fields[email_fields %in% names(metadata_in)]
 
-  metadata_in[email_fields] <- purrr::map(metadata_in[email_fields], function(x) {
-    paste(purrr::map(x, extract_from_list, keys = c('email')), collapse = '; ')
+  # Handle variables pulling for multiples JSON keys
+  email_fields <- email_fields[email_fields %in% names(metadata_list)]
+
+  metadata_list[email_fields] <- purrr::map(metadata_list[email_fields], function(x) {
+    out <- paste(purrr::map(x, extract_from_list, keys = c('email')), collapse = '; ')
+    out <- stringr::str_replace_all(out, pattern = '^; ?|;$|; $', replacement = '')
   })
 
   # Handle extraction of value that follow "name" keys
-  name_fields <- name_fields[name_fields %in% names(metadata_in)]
+  name_fields <- name_fields[name_fields %in% names(metadata_list)]
 
-  metadata_in[name_fields] <- purrr::map(metadata_in[name_fields], function(x) x[[1]]$name)
+  metadata_list[name_fields] <- purrr::map(metadata_list[name_fields], function(x) x[[1]]$name)
 
   # Handle extraction of date values
 
   field <- "field_wbddh_start_date"
-  if (!is.null(metadata_in[[field]])) {
-    keep <- purrr::map_lgl(metadata_in[[field]], function(x) x$event %in% c("start", "single"))
-    metadata_in[[field]]  <- metadata_in[[field]][keep]
-    metadata_in[[field]] <- metadata_in[[field]][[1]]$date
+  if (!is.null(metadata_list[[field]])) {
+    keep <- purrr::map_lgl(metadata_list[[field]], function(x) x$event %in% c("start", "single"))
+    metadata_list[[field]]  <- metadata_list[[field]][keep]
+    metadata_list[[field]] <- metadata_list[[field]][[1]]$date
   }
 
   field <- "field_wbddh_end_date"
-  if (!is.null(metadata_in[[field]])) {
-    keep <- purrr::map_lgl(metadata_in[[field]], function(x) x$event %in% c("end", "single"))
-    metadata_in[[field]]  <- metadata_in[[field]][keep]
-    metadata_in[[field]] <- metadata_in[[field]][[1]]$date
+  if (!is.null(metadata_list[[field]])) {
+    keep <- purrr::map_lgl(metadata_list[[field]], function(x) x$event %in% c("end", "single"))
+    metadata_list[[field]]  <- metadata_list[[field]][keep]
+    metadata_list[[field]] <- metadata_list[[field]][[1]]$date
   }
 
   # CHECK: List has been correctly flattened
-  assertthat::validate_that(all(purrr::map_int(metadata_in, purrr::depth) == 1),
+  assertthat::validate_that(all(purrr::map_int(metadata_list, purrr::depth) == 1),
                             msg = "Some elements of the list have not been flattened correctly")
 
-  return(metadata_in)
+  return(metadata_list)
 
 }
