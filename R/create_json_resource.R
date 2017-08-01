20492bb7dee394bff2f5c1d3940ef8b0cd8739a8
Tony Fujs <tony.fujs@gmail.com>
2017-08-01 18:03:05 -0400

    update resource_type code

--- R/create_json_resource.R
+++ R/create_json_resource.R
@@ -17,7 +17,7 @@ create_json_resource <- function(metadata_list, json_template = mdlibtoddh::json
   json_template$field_wbddh_data_class$und$tid <- safe_unbox(metadata_list$field_wbddh_data_class) # data classification
   json_template$field_exception_s_$und$tid <- safe_unbox(metadata_list$field_exception_s_)
   json_template$field_link_api$und$url <- safe_unbox(metadata_list$field_link_api)
-  json_template$field_wbddh_resource_type$und$tid <- safe_unbox('1007')
+  json_template$field_wbddh_resource_type$und$tid <- safe_unbox('1192')
 
     # remove empty elements
   # to_keep <- names(metadata_list[!purrr::map_int(metadata_list, length) == 0])
