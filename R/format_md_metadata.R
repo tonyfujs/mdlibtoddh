03c3631f4ef0ea7b6f0b14b0636e6c11b15621b5
Karthik Ramanathan Dhanalakshmi Ramanathan <kdhanalakshmiram@worldbank.org>
2017-09-26 16:29:36 -0400

    modify format_md_metadata() to not map null character fields

--- R/format_md_metadata.R
+++ R/format_md_metadata.R
@@ -28,7 +28,8 @@ format_md_metadata <- function(metadata_in,
   metadata_in[date_fields] <- purrr::map(metadata_in[date_fields], timestamp_to_ddhdate)
 
   # Format text fields
-  metadata_in[text_fields] <- purrr::map(metadata_in[text_fields], format_text)
+  is.notnull = function(x)!is.null(x)
+  metadata_in[text_fields] <- purrr::map_if(metadata_in[text_fields], is.notnull, format_text)
 
   return(metadata_in)
 }
