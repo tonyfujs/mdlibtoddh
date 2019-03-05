4641a8b4aa1e90c1e675a74686d4ddbf038e5b53
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-03-05 11:20:44 -0500

    cleaned code

--- R/update_existing_dataset.R
+++ R/update_existing_dataset.R
@@ -66,7 +66,7 @@ update_existing_dataset <- function(md_internal_id, md_token, master,
                                                credentials = credentials)
   nid_res <- unlist(ddhconnect::get_resource_nids(metadata_dataset))
   
-  #makesure resource is Microdata Query Tool
+  #Makesure resource is Microdata Landing Page
   if(length(nid_res) > 1){
     nid_res <- resource_check(as.list(nid_res))
   }
