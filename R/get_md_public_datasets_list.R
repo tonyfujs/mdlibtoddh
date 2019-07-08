64bdc86d41eb2158844051ea2921d62cbe31d292
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-07-08 12:34:46 -0400

    small fix

--- R/get_md_public_datasets_list.R
+++ R/get_md_public_datasets_list.R
@@ -9,7 +9,7 @@
 get_md_public_datasets_list <- function(token) {
   resp <- mdlibconnect::get_public_survey_list(token = token)
   md_external_refid <- purrr::map_chr(resp, "idno")
-  md_external_changed <- purrr::map_chr(resp, "changed")
+  md_updated <- purrr::map_chr(resp, "changed")
   md_external_id <- purrr::map_chr(resp, "id")
 
   out <- data.frame(md_external_refid, md_updated, md_external_id, stringsAsFactors = FALSE)
