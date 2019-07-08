4525cae503495206c257e566b5c85e7002038fb5
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-07-08 12:45:18 -0400

    reverted changes

--- R/get_md_public_datasets_list.R
+++ R/get_md_public_datasets_list.R
@@ -9,10 +9,10 @@
 get_md_public_datasets_list <- function(token) {
   resp <- mdlibconnect::get_public_survey_list(token = token)
   md_external_refid <- purrr::map_chr(resp, "idno")
-  md_updated <- purrr::map_chr(resp, "changed")
+  md_external_changed <- purrr::map_chr(resp, "changed")
   md_external_id <- purrr::map_chr(resp, "id")
 
-  out <- data.frame(md_external_refid, md_updated, md_external_id, stringsAsFactors = FALSE)
+  out <- data.frame(md_external_refid, md_external_changed, md_external_id, stringsAsFactors = FALSE)
 
   return(out)
 }
